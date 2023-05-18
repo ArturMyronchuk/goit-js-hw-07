3ceb994365a485918f1eeeaa34d365ba2b211061
Artur Myronchuk <rolax223@gmail.com>
2023-05-18 19:01:22 +0300

    number2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector(".gallery");
+const cardMarkup = createGalleryItem(galleryItems);
+galleryItemsEl.insertAdjacentHTML("beforeend", cardMarkup);
+
+function createGalleryItem(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`;
+    })
+    .join("");
+  return markup;
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
